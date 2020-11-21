6bb096fe3f7fdcb9765107fdb137593d10202c62
Emily Spoldi <emily.l.spoldi@gmail.com>
2020-11-20 21:16:03 -0500

    added timer

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //Nav Bar
 $("body").append("<nav>");
 
+    //Timer
+    var seconds = 10;
+    $("nav").append(`<p>Time: ${seconds}`);
+
+    function setTime() {
+        var timerInterval = setInterval(function() {
+            seconds--;
+            $("p")[0].textContent = `Time: ${seconds}`;
+        
+        if(seconds === 0) {
+            clearInterval(timerInterval);
+        }
+        }, 1000);
+    }
+    
+    setTime();
+
+
 //Quiz Section
 $("body").append("<section>");
 
